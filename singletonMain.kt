529a355f09e0ec9109783f21279afef8ef9dda2d
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-01-24 18:31:25 -0300

    Update singletonMain

--- singletonMain.kt
+++ singletonMain.kt
@@ -1,19 +1,19 @@
 class User(val id: Int, val name: String)
 
-/*
- * No Kotlin, a declaração de um objeto (por meio da palavra-chave object)
- * é uma maneira concisa e eficaz de implementar o padrão Singleton.
- */
 object UserManager {
 
     private val users = mutableListOf<User>()
-
-    fun addUser(name: String) {
-        TODO("Implementar a lógica de adicionar um novo usuário na lista mutável $users.")
+    var it = 1
+    fun addUser(nome: String) {
+        users.add(User(it,nome))
+        it++
     }
 
     fun listUsers() {
-        TODO("Implementar a impressão dos $users, seguindo o padrão definido no enunciado.")
+        for (user in users){
+            println("${user.id} - ${user.name}")
+        }
+
     }
 }
 
